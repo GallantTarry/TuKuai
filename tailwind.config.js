0911ddcb5e0045a3ca7d59f735f68c1b7267e4d9
TuKuai <w1961459892@gmail.com>
2026-09-16 19:37:33 +0800

    916

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        pixel: ['"Silkscreen"', '"ChinesePixel"', 'cursive']
+      }
+    }
+  },
+  plugins: [],
+}
